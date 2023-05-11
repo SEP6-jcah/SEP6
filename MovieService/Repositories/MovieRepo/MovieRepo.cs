8a20c7c018c60277c25975f446e2befc4b9d382d
AlleyCatRacer <74299218+AlleyCatRacer@users.noreply.github.com>
2023-05-11 22:06:32 +0200

    Movie repo
    - Uncommented code

--- MovieService/Repositories/MovieRepo/MovieRepo.cs
+++ MovieService/Repositories/MovieRepo/MovieRepo.cs
@@ -16,19 +16,19 @@ namespace MovieService.Repositories.MovieRepo
 
         public async Task<IList<Movie>> GetMoviesAsync()
         {
-            // if (persistence.movies != null)
-            // {
-            //     return persistence.movies.ToList();
-            // }
-            // else
-            // {
+            if (persistence.movies != null)
+            {
+                return persistence.movies.ToList();
+            }
+            else
+            {
                 var testMovie = new Movie{
                     Id = 123,
                     Title = "Test",
                     Year = 2023
                 };
                 return new List<Movie>{testMovie};
-            // }
+            }
         }
 
 
