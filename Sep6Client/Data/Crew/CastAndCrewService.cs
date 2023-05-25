29d8193744aa81406dd1ef2bdfd060798b184c41
carrolljody <75698201+carrolljody@users.noreply.github.com>
2023-05-25 22:39:49 +0200

    move api key and base uri to app settings
    quick fix

--- Sep6Client/Data/Crew/CastAndCrewService.cs
+++ Sep6Client/Data/Crew/CastAndCrewService.cs
@@ -5,23 +5,27 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text.Json;
 using System.Threading.Tasks;
+using Sep6Client.Data.DataHelper;
 using Sep6Client.Data.DataHelper.Mappers;
 using Sep6Client.Data.DataHelper.Wrappers;
 using Sep6Client.Model;
+using Microsoft.Extensions.Options;
 
 namespace Sep6Client.Data.Crew
 {
     public class CastAndCrewService : ICastAndCrewService
     {
         private readonly HttpClient client;
-        private const string BaseUri = "https://api.themoviedb.org/3/";
-        private const string ApiKey = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzM2YxNmY4ZDRmYTE4ZDhmZTY2MTZlNDcyYWJhMjNhMCIsInN1YiI6IjY0NjVkNjA3MDA2YjAxMDEwNTg4Y2ZkNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.RCfsqHoolmtYr-oCW7DLtmdlR1zqfeJz2NUkPvgBQZg";
+        private string baseUri;
+        private string apiKey;
         private readonly JsonSerializerOptions options;
-        //https://api.themoviedb.org/3/movie/640146/credits
-        public CastAndCrewService()
+
+        public CastAndCrewService(IOptions<TmdbSettings> tmdbSettings)
         {
             client = new HttpClient();
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", ApiKey);
+            baseUri = tmdbSettings.Value.BaseUri;
+            apiKey = tmdbSettings.Value.ApiKey;
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiKey);
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
             options = new JsonSerializerOptions
             {
@@ -32,8 +36,8 @@ namespace Sep6Client.Data.Crew
 
         private async Task<CastAndCrewListResult> GetCastAndCrewAsync(int movieId)
         {
-            var response = await client.GetAsync($"{BaseUri}movie/{movieId}/credits");
-            Console.WriteLine($"Sending cast and crew query to: {BaseUri}movie/{movieId}/credits");
+            var response = await client.GetAsync($"{baseUri}movie/{movieId}/credits");
+            Console.WriteLine($"Sending cast and crew query to: {baseUri}movie/{movieId}/credits");
             
             if (!response.IsSuccessStatusCode)
             {
@@ -55,7 +59,6 @@ namespace Sep6Client.Data.Crew
 
             try
             {
-                // Console.WriteLine($"\n\n\n__________Count of crew l49: {response.Crew.Count}__________\n\n\n");
                 crew.AddRange(response.Crew.Select(CrewMemberMapper.ToCrewMember));
             }
             catch (Exception e)
---
+++ Sep6Client/Data/DataHelper/TmdbSettings.cs
@@ -0,0 +1,8 @@
+namespace Sep6Client.Data.DataHelper
+{
+    public class TmdbSettings
+    {
+        public string ApiKey { get; set; }  = String.Empty;
+        public string BaseUri { get; set; }  = String.Empty;
+    }
+}
